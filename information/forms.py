14e7385f77e1338f710d5397c4545b189ccef747
leopard1982 <99190737+leopard1982@users.noreply.github.com>
2023-05-09 06:58:56 +0700

    great maning

--- information/forms.py
+++ information/forms.py
@@ -4,10 +4,6 @@ from django import forms
 
 #to create new user
 class SignUpForm(UserCreationForm):
-	# email = forms.EmailField(label="Email:",widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Your Email'}))
-	# first_name = forms.CharField(label="First Name",max_length=100,widget=forms.TextInput(attrs={'class':'form-control','placeholder':'First Name'}))
-	# last_name = forms.CharField(label="Last Name",max_length=100,widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Last Name'}))
-
 	class Meta:
 		model = User
 		fields = ['username','email','first_name','last_name','password1','password2']
